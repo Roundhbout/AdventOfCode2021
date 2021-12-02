3bdcf4582b6b7e7fc91e6df8a312f04061fea07e
Zach Kelley <kelley.z@northeastern.edu>
2021-12-01 19:32:44 -0500

    neat little trick to slightly improve pt2 (don't need sums)

--- Day1/day1.py
+++ Day1/day1.py
@@ -3,13 +3,15 @@ f = open("input.txt", "r")
 
 depths = list(map(int, f.read().split()))
 
+# count times an item in depths is greater than the item before it
 def depth_increases(depths):
     return [depths[i] > depths[i - 1] for i in range(1, len(depths))].count(True)
 
+"""since across comparisons there will always be two common values (the middle two)
+we can just see if depths[i] is greater than depths[i - 3]
+(i.e. the value that changes from AAA to BBB)"""
 def sliding_window_depth_increases(depths):
-    start = time.time()
-    windows = [sum(depths[i - 3 : i]) for i in range(3, len(depths) + 1)]
-    ans = [windows[i] > windows[i - 1] for i in range(1, len(windows))].count(True)
+    ans = [depths[i] > depths[i - 3] for i in range(3, len(depths))].count(True)
     return ans
 
 
